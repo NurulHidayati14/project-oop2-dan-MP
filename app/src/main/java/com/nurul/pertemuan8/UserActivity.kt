236216672a2ec043bcb9249b5cf69b31dc5381ef
NurulHidayati14 <56246031+NurulHidayati14@users.noreply.github.com>
2020-12-23 12:20:25 +0700

    ganti tabel user dan menambahkan data tabel user

---
+++ app/src/main/java/com/nurul/pertemuan8/UserActivity.kt
@@ -0,0 +1,66 @@
+package com.nurul.pertemuan8
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nurul.pertemuan8.Database.AppRoomDB
+import com.nurul.pertemuan8.Database.Constant
+import com.nurul.pertemuan8.Database.User
+import kotlinx.android.synthetic.main.activity_user.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class UserActivity : AppCompatActivity() {
+
+    val db by lazy { AppRoomDB(this) }
+    lateinit var userAdapter: UserAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+        setupListener()
+        setupRecyclerView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.IO).launch {
+            val allUser = db.userDao().getAllUser()
+            Log.d("UserActivity", "dbResponse: $allUser")
+            withContext(Dispatchers.Main) {
+                userAdapter.setData(allUser)
+            }
+        }
+    }
+
+    fun setupListener() {
+        btn_createUser.setOnClickListener {
+            intentEdit(0, Constant.TYPE_CREATE)
+        }
+    }
+
+    fun setupRecyclerView() {
+        userAdapter = UserAdapter(arrayListOf(), object: UserAdapter.OnAdapterListener {
+            override fun onClick(user: User) {
+                intentEdit(user.id, Constant.TYPE_READ)
+            }
+
+        })
+        list_user.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = userAdapter
+        }
+    }
+
+    fun intentEdit(userId: Int, intentType: Int ) {
+        startActivity(
+            Intent(applicationContext, EditUserActivity::class.java)
+                .putExtra("intent_id", userId)
+                .putExtra("intent_type", intentType)
+        )
+    }
+}
